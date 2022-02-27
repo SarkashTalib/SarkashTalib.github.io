58363a2206c5db9bdb0daf616556c69bdebf6e45
Sarkash Talib <sarkash.talib@gmail.com>
2022-02-27 02:41:05 +0000

    Responsive home page

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: [
+    "./dist/index.html",
+  ],
+  theme: {
+    container: {
+      center: true,
+      maxwidth: '900rem',
+      flex: true,    
+    },
+    colors: {
+      'background': '#EFECEA',
+      'amber': '#DAA355',
+      'alert': '#F22F4D',
+      'light': '#FFFFFF',
+      'grayish': '#625959',
+      'button': '#DBAC94',
+      'dark': '#000000'
+    },
+    fontFamily: {
+      'lora': ['Lora'],
+      'opensans': ['Open Sans'],
+      'roboto': ['Roboto'],
+      'rubik': ['Rubik'],
+      'alexbrush': ['Alex Brush']
+
+    },
+
+  },
+  plugins: [require('@tailwindcss/aspect-ratio'),],
+
+};
